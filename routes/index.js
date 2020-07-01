c001b4c7ab0957e9d733fa9f85c6f86054841765
Carlton Joseph <carlton.joseph@gmail.com>
2020-07-01 13:42:44 -0700

    add express sequelize crud operations

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const controllers = require("../controllers");
+
+const router = Router();
+
+router.get("/", (req, res) => res.send("Welcome"));
+router.get("/posts", controllers.getAllPosts);
+router.get("/posts/:postId", controllers.getPostById);
+router.post("/posts", controllers.createPost);
+router.post("/posts/:postId", controllers.updatePost);
+router.delete("/posts/:postId", controllers.deletePost);
+
+module.exports = router;
